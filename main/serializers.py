a99d963d045d7ffea6f086f575e8089003bd7c96
4eckah78 <4eckah78>
2020-10-30 23:30:51 +0300

    initial commit

---
+++ main/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+
+User = get_user_model()
+
+
+class UserListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "email", "first_name", "last_name", "is_superuser", "table_name"]
+        extra_kwargs = {'email':{"required": False}, 'table_name':{"required": False} }
+
+
+class MyUserSerializer(serializers.ModelSerializer):
+    # token = serializers.CharField(source=Token.key)
+    class Meta:
+        model = User
+        fields = "__all__"
+
+        
+
+class MyTokenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Token
+        fields = ["user", "key"]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from django.http import JsonResponse
+from .models import User
+from .serializers import *
+from rest_framework.authtoken.models import Token
 
-# Create your views here.
+
+class GetAllUsers(generics.ListAPIView):
+    serializer_class = UserListSerializer
+    queryset = User.objects.all()
+
+
+class PutGetDeleteOneUser(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = UserListSerializer
+    queryset = User.objects.all()
+    # def put(self, request, pk):
+    #     user = User.objects.filter(id=pk)
+
+        

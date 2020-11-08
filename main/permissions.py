cfdefbe196811443cd03025ad54521db877e922d
4eckah78 <4eckah78>
2020-11-08 17:16:40 +0300

    remake permissions

--- main/permissions.py
+++ main/permissions.py
@@ -3,18 +3,13 @@ from rest_framework.authtoken.models import Token
 from .models import User
 
 
-class IsOwnerOrReadOnlyOrIsAdmin(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS or request.user.is_superuser:
-            return True
-        if len(Token.objects.filter(user=obj.id)) == 0:
-            return false 
-        obj_token = Token.objects.get(user=obj.id).key
-        request_token = request.headers['Authorization'][6:]
-        return obj_token == request_token
-
-
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.is_superuser
      
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj == request.user
